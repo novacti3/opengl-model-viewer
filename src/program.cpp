b758fc84cdd48798ef2a50a6cb02221b1a84c356
novacti3 <32226878+novacti3@users.noreply.github.com>
2021-09-16 22:00:23 +0200

    Initial commit

---
+++ src/program.cpp
@@ -0,0 +1,36 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl;
+
+static unsigned int constexpr WINDOW_WIDTH = 1270; 
+static unsigned int constexpr WINDOW_HEIGHT = 720;
+static const std::string WINDOW_TITLE = "My OpenGL Program";
+
+int main()
+{
+    if(!glfwInit())
+    {
+        LOG("GLFW failed to initialize...");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE.c_str(), NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        LOG("glad failed to initialize...");
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    
+    return 0;
+}
