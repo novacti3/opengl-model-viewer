1f4181442f224bcb833a87ce01f4134f9347f1e7
Jan Šaler <honza@pop-os.localdomain>
2021-09-30 10:51:05 +0200

    rectangle

--- src/program.cpp
+++ src/program.cpp
@@ -38,19 +38,31 @@ int main()
 
     float vertices[] =
     {
-        0.0f, 0.5f, // top
-        -0.5f, -0.5f, // left
-        0.5f, -0.5f // right
+        -0.5f, 0.5f, // top left
+        -0.5f, -0.5f, // bot left
+        0.5f, -0.5f, // bot right
+        0.5f, 0.5f // top right
     };
 
-    unsigned int VAO, VBO;
+    unsigned int indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    unsigned int VAO, VBO, EBO;
 
     GL_CALL(glad_glGenVertexArrays(1, &VAO));
     GL_CALL(glad_glGenBuffers(1, &VBO));
+    GL_CALL(glad_glGenBuffers(1, &EBO));
 
     GL_CALL(glad_glBindVertexArray(VAO));
+
     GL_CALL(glad_glBindBuffer(GL_ARRAY_BUFFER, VBO));
     GL_CALL(glad_glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
+
+    GL_CALL(glad_glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO));
+    GL_CALL(glad_glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
 
     GL_CALL(glad_glVertexAttribPointer(0, 2, GL_FLOAT, false, 2*sizeof(float), nullptr));
     GL_CALL(glad_glEnableVertexAttribArray(0));
@@ -74,7 +86,8 @@ int main()
 
         GL_CALL(glad_glBindVertexArray(VAO));
         resShader->get()->Bind();
-        GL_CALL(glad_glDrawArrays(GL_TRIANGLES, 0, 3));
+        // GL_CALL(glad_glDrawArrays(GL_TRIANGLES, 0, 3));
+        GL_CALL(glad_glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
         resShader->get()->Unbind();
         GL_CALL(glad_glBindVertexArray(0));
     }
